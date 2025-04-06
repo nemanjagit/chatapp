00d4535c6cb2756fa1e4698f354d19173b8665d9
nemanjagit <jovanovicnemanja263@gmail.com>
2025-04-06 12:21:30 +0200

    Add profile update functionality with Cloudinary integration and JWT protection

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import cookieParser from "cookie-parser";
+
+// This middleware checks if the user is authenticated by verifying the JWT token
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt;
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized - No token provided" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decoded) {
+            return res.status(401).json({ message: "Unauthorized - Invalid token" });
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized - User not found" });
+        }
+        req.user = user; // Attach the user to the request object
+
+        next();
+
+    } catch (error) {
+        console.log("Error in protectRoute middleware", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
