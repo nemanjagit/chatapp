55b3ac9e31002fb4f3ff3be659b8c9e882a62c91
nemanjagit <jovanovicnemanja263@gmail.com>
2025-04-07 15:47:37 +0200

    Implement messaging functionality on backend side with message model, controller, and routes. Realtime messaging functionality left for todo.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -10,7 +11,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+
 
 app.listen(PORT , () => {
     console.log("Server is running on port: "+ PORT);
