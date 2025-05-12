8634928c182be03693914deff38b715d476ba354
nemanjagit <jovanovicnemanja263@gmail.com>
2025-05-12 23:29:54 +0200

    chore: consolidate .gitignore files and add environment variable support; update package.json for build and start scripts

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,12 @@ import {connectDB} from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Set the body size limit to 1MB
 app.use(express.json({ limit: '1mb' })); // Adjust the size limit here
@@ -20,6 +23,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "/frontend/dist/index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on port: " + PORT);
